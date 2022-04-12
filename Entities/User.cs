0e05f6b91028e82f4c010cb2d039869e4e89ddc4
waqasbt <waqas-dev@outlook.com>
2022-04-12 22:08:48 +0500

    Added Comments

--- Entities/User.cs
+++ Entities/User.cs
@@ -9,10 +9,19 @@ namespace Entities
 {
     public class User : BaseEntity
     {
+        // First name of the user.
         public string FirstName { get; set; }
+
+        // Last name of the user.
         public string LastName { get; set; }
+
+        // Email Id of the user.
         public string Email { get; set; }
+
+        // Profile picture URL of user.
         public string ProfilePicUrl { get; set; }
+
+        // One USer might have 1 or more Account (1:Many relationship)
         public virtual Account Account { get; set; }
     }
 }
